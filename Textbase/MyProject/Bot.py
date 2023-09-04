55e4cac725559ae6a9a1a855e50dafc4cbcb9beb
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2023-09-04 08:54:55 +0530

    Add files via upload

---
+++ Textbase/MyProject/Bot.py
@@ -0,0 +1,54 @@
+from textbase import bot, Message
+from textbase.models import OpenAI
+from typing import List
+from gtts import gTTS
+import os
+import subprocess
+
+# Load your OpenAI API key
+OpenAI.api_key = "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"
+
+# Prompt for GPT-3.5 Turbo
+SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like.
+The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a
+pleasant chat!
+"""
+
+@bot()
+def on_message(message_history: List[Message], state: dict = None):
+    # Generate GPT-3.5 Turbo response
+    bot_response = OpenAI.generate(
+        system_prompt=SYSTEM_PROMPT,
+        message_history=message_history, # Assuming history is the list of user messages
+        model="gpt-3.5-turbo",
+    )
+
+    # Convert the text response to audio
+    tts = gTTS(bot_response, lang='en')
+    tts.save("bot_response.mp3")
+
+    # Play the audio response on Windows
+    subprocess.Popen(['start', 'bot_response.mp3'], shell=True)
+
+    response = {
+        "data": {
+            "messages": [
+                {
+                    "data_type": "STRING",
+                    "value": bot_response
+                }
+            ],
+            "state": state
+        },
+        "errors": [
+            {
+                "message": ""
+            }
+        ]
+    }
+
+    return {
+        "status_code": 200,
+        "response": response
+    }
+
